ae48b53529a4465ed265abbec779c7a46598134b
Tobias Ollmann <tobias.ollmann@instahelp.me>
2023-11-20 13:32:25 +0100

    feat: add analytics server

--- pkg/service/wsprotocol.go
+++ pkg/service/wsprotocol.go
@@ -142,6 +142,7 @@ func (c *WSSignalConnection) WriteResponse(msg *livekit.SignalResponse) (int, er
 	if err != nil {
 		return 0, err
 	}
+	logger.Infow("Sending response data over network", "length", len(payload), "json", c.useJSON)
 
 	return len(payload), c.conn.WriteMessage(msgType, payload)
 }
@@ -164,6 +165,7 @@ func (c *WSSignalConnection) WriteServerMessage(msg *livekit.ServerMessage) (int
 	if err != nil {
 		return 0, err
 	}
+	logger.Infow("Sending data over network", "length", len(payload), "json", c.useJSON)
 
 	return len(payload), c.conn.WriteMessage(msgType, payload)
 }
