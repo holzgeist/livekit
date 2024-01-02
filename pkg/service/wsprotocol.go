7223ae5eec7dd48213babacc6df2b64a664c8221
Tobias Ollmann <tobias.ollmann@instahelp.me>
2024-01-02 09:21:59 +0100

    fix: cleanup debug changes

--- pkg/service/wsprotocol.go
+++ pkg/service/wsprotocol.go
@@ -135,7 +135,6 @@ func (c *WSSignalConnection) WriteResponse(msg *livekit.SignalResponse) (int, er
 	if err != nil {
 		return 0, err
 	}
-	logger.Infow("Sending response data over network", "length", len(payload), "json", c.useJSON)
 
 	return len(payload), c.conn.WriteMessage(msgType, payload)
 }
@@ -158,7 +157,6 @@ func (c *WSSignalConnection) WriteServerMessage(msg *livekit.ServerMessage) (int
 	if err != nil {
 		return 0, err
 	}
-	logger.Infow("Sending data over network", "length", len(payload), "json", c.useJSON)
 
 	return len(payload), c.conn.WriteMessage(msgType, payload)
 }
